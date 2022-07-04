59ce0f6fe15886c194ac3ba755a500a6423f4f0f
Martin Marconcini <martin.marconcini.rodriguez@nl.abnamro.com>
2022-07-04 14:20:05 +0200

    Update dependencies to July 2022 standards.

--- app/src/main/java/com/neutobo/recyclerviewwithads/MainActivity.kt
+++ app/src/main/java/com/neutobo/recyclerviewwithads/MainActivity.kt
@@ -3,9 +3,11 @@ package com.neutobo.recyclerviewwithads
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import com.neutobo.recyclerviewwithads.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private var adapter: ThingAdapterWithAds = ThingAdapterWithAds()
     private var repository: FakeDataRepository = FakeDataRepository()
@@ -13,11 +15,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         layoutManager = GridLayoutManager(this, 2)
 
-        mainRecyclerView.layoutManager = layoutManager
-        mainRecyclerView.adapter = adapter
+        binding.mainRecyclerView.layoutManager = layoutManager
+        binding.mainRecyclerView.adapter = adapter
 
         layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
             override fun getSpanSize(position: Int): Int {
